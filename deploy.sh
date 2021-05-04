6490b90ac26bf7276fc2e6b44993930952f99992
wisehackermonkey <oranbusiness@gmail.com>
2021-05-04 19:46:35 +0000

    [add] mvp + automated fix

--- deploy.sh
+++ deploy.sh
@@ -19,13 +19,13 @@ git config --global user.name "wisehackermonkey"
 command -v docker >/dev/null 2>&1 ||
 { echo >&2 "docker is not installed. Installing..";
 # install docker
-sudo apt update
+sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce  -y
 sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 su - ${USER}
@@ -46,43 +46,46 @@ command -v docker-compose >/dev/null 2>&1 ||
 
 # make default download location for my github repos
 cd ~
-mkdir ~/github
+[ ! -d "${HOME}//github" ] && mkdir ~/github
 cd ~/github
 
 # download all my repos
 # [Check if a directory exists in Linux or Unix shell - nixCraft](https://www.cyberciti.biz/faq/check-if-a-directory-exists-in-linux-or-unix-shell/)
-[ ! -d "${HOME}/github/website" ] && git clone https://github.com/wisehackermonkey/website.git && echo "website\n\n\n"
-[ ! -d "${HOME}/github/website-router" ] && git clone https://github.com/wisehackermonkey/website-router.git && echo "website-router\n\n\n"
-[ ! -d "${HOME}/github/chirpweb" ] && git clone https://github.com/wisehackermonkey/chirpweb.git && echo "chirpweb\n\n\n"
-[ ! -d "${HOME}/github/remash" ] && git clone https://github.com/wisehackermonkey/remash.git && echo "remash\n\n\n"
-[ ! -d "${HOME}/github/shitty-90s-website" ] && git clone https://github.com/wisehackermonkey/shitty-90s-website.git && echo "shitty-90s-website\n\n\n"
-[ ! -d "${HOME}/github/blank-black-website" ] && git clone https://github.com/wisehackermonkey/blank-black-website.git && echo "blank-black-website\n\n\n"
-[ ! -d "${HOME}/github/webtorrent-demo" ] && git clone https://github.com/wisehackermonkey/webtorrent-demo.git && echo "webtorrent-demo\n\n\n"
-[ ! -d "${HOME}/github/pixel-artist" ] && git clone https://github.com/wisehackermonkey/pixel-artist.git && echo "pixel-artist\n\n\n"
-[ ! -d "${HOME}/github/business_card_puzzle" ] && git clone https://github.com/wisehackermonkey/business_card_puzzle.git && echo "business_card_puzzle\n\n\n"
+[ ! -d "${HOME}/github/website" ] && git clone https://github.com/wisehackermonkey/website.git && printf "website\n\n\n"
+[ ! -d "${HOME}/github/website-router" ] && git clone https://github.com/wisehackermonkey/website-router.git && printf "website-router\n\n\n"
+[ ! -d "${HOME}/github/chirpweb" ] && git clone https://github.com/wisehackermonkey/chirpweb.git && printf "chirpweb\n\n\n"
+[ ! -d "${HOME}/github/remash" ] && git clone https://github.com/wisehackermonkey/remash.git && printf "remash\n\n\n"
+[ ! -d "${HOME}/github/shitty-90s-website" ] && git clone https://github.com/wisehackermonkey/shitty-90s-website.git && printf "shitty-90s-website\n\n\n"
+[ ! -d "${HOME}/github/blank-black-website" ] && git clone https://github.com/wisehackermonkey/blank-black-website.git && printf "blank-black-website\n\n\n"
+[ ! -d "${HOME}/github/webtorrent-demo" ] && git clone https://github.com/wisehackermonkey/webtorrent-demo.git && printf "webtorrent-demo\n\n\n"
+[ ! -d "${HOME}/github/pixel-artist" ] && git clone https://github.com/wisehackermonkey/pixel-artist.git && printf "pixel-artist\n\n\n"
+[ ! -d "${HOME}/github/business_card_puzzle" ] && git clone https://github.com/wisehackermonkey/business_card_puzzle.git && printf "business_card_puzzle\n\n\n"
 
 
 
-echo "manual process\n\n\n"
+printf "manual process\n\n\n"
 docker login
 
-echo "download private ghost blog backup data\n\n\n"
+printf "download private ghost blog backup data\n\n\n"
 [ ! -d "${HOME}/github/blog-backup" ] && git clone https://github.com/wisehackermonkey/blog-backup.git
 
-# echo "website-router  [Starting]\n\n\n"
-# cd ./website && docker-compose up
-
-##########
-echo "start all the services run\n\n\n"
-
-# echo "XXXXXXXXX [Starting]\n\n\n"
-# docker-compose -f ./XXXXXXXXX/docker-compose.yml up -d && echo "XXXXXXXXX [Done]\n"
 
 
-echo "chirpweb [Starting]\n\n\n" &&             cd ~/github/chirpweb && docker-compose up -d && echo "chirpweb [Done]\n\n"
-echo "remash [Starting]\n\n\n" &&               cd ~/github/remash && docker-compose -f ./docker-compose.deploy.yml up -d && echo "remash [Done]\n\n\n"
-echo "shitty-90s-website [Starting]\n\n\n" &&   cd ~/github/shitty-90s-website && docker-compose up -d && echo "shitty-90s-website [Done]\n\n\n"
-echo "blank-black-website [Starting]\n\n\n" &&  cd ~/github/blank-black-website && docker-compose up -d && echo "blank-black-website [Done]\n\n\n" 
-# echo "webtorrent-demo [Starting]\n\n\n" &&      cd ~/github/webtorrent-demo && docker-compose build && docker-compose up -d && echo "webtorrent-demo [Done]\n\n\n"
-echo "pixel-artist [Starting]\n\n\n" &&         cd ~/github/pixel-artist && docker-compose up -d && echo "pixel-artist [Done]\n\n\n"
-echo "business_card_puzzle [Starting]\n\n\n" && cd ~/github/business_card_puzzle && docker-compose up -d && echo "business_card_puzzle [Done]\n\n\n"
+######  ####
+printf "start all the services run\n\n\n"
+
+# printf "XXXXXXXXX [Starting]\n\n\n"
+# docker-compose -f ./XXXXXXXXX/docker-compose.yml up -d && printf "XXXXXXXXX [Done]\n"
+
+# printf "webtorrent-demo [Starting]\n\n\n" &&      cd ~/github/webtorrent-demo && docker-compose build && docker-compose up -d && printf "webtorrent-demo [Done]\n\n\n"
+printf "website-router [Starting]\n\n\n" &&             cd ~/github/website-router && docker-compose up -d && printf "website-router [Done]\n\n"
+
+printf "chirpweb [Starting]\n\n\n" &&             cd ~/github/chirpweb && docker-compose up -d && printf "chirpweb [Done]\n\n"
+printf "shitty-90s-website [Starting]\n\n\n" &&   cd ~/github/shitty-90s-website && docker-compose up -d && printf "shitty-90s-website [Done]\n\n\n"
+printf "blank-black-website [Starting]\n\n\n" &&  cd ~/github/blank-black-website && docker-compose up -d && printf "blank-black-website [Done]\n\n\n"
+printf "business_card_puzzle [Starting]\n\n\n" && cd ~/github/business_card_puzzle && docker-compose up -d && printf "business_card_puzzle [Done]\n\n\n"
+## Special snowflake ones
+printf "website [Starting]\n\n\n" &&             cd ~/github/website && docker-compose -f ./docker-compose.deploy.yml up -d && printf "website [Done]\n\n"
+printf "remash [Starting]\n\n\n" &&               cd ~/github/remash && docker-compose -f ./docker-compose.deploy.yml up -d && printf "remash [Done]\n\n\n"
+printf "pixel-artist [Starting]\n\n\n" &&         cd ~/github/pixel-artist && docker-compose -f ./docker-compose.deploy.yml up -d && printf "pixel-artist [Done]\n\n\n"
+printf "blog [Starting]\n\n\n" &&         cd ~/github/blog-backup && docker-compose -f ./docker-compose.deploy.yml up -d && printf "blog [Done]\n\n\n"
